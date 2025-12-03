95e088cbce546f3e6b06ad47896e4909e3ea4b0e
Neel bhavnagari <167882938+neelbhavnagari@users.noreply.github.com>
2025-12-03 23:48:07 +0530

    Collection
    Arraylist & Set

---
+++ demo.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Set;
+
+// public class Simple {
+//     public static void main(String[] args) {
+//         List<String> list = new ArrayList<>();
+//         list.add("A");
+//         list.add("B");
+//         list.add("C");
+//         System.out.println(list);
+//     }
+
+// }
+
+public class demo {
+    public static void main(String[] args) {
+        Set <Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        System.out.println(set);
+    }
+
+}
